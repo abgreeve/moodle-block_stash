0f1b80a2c2e369099791dca3780b69d1ded9b046
Adrian Greeve <adrian@moodle.com>
2023-10-19 10:38:09 +0800

    Version and release information bump.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023100903;
+$plugin->version   = 2023102000;
 $plugin->requires  = 2022112802; // Moodle 4.1.2.
 $plugin->component = 'block_stash';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.0.2';
+$plugin->release   = '2.0.3';
